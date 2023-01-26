9faf5dd5a6ab92fd4fa2248151668e98c7417819
arcCoelho <arc-coelho@hotmail.com>
2023-01-25 22:29:34 -0300

    created project

---
+++ backend/src/main/java/com/sellerpro/sellerproapi/SellerproapiApplication.java
@@ -0,0 +1,13 @@
+package com.sellerpro.sellerproapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerproapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerproapiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sellerpro/sellerproapi/SellerproapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sellerpro.sellerproapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerproapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
